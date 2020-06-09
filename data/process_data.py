65ada036102a39362737279117f9a138103ffe3f
clarissabachmann <51957412+clarissabachmann@users.noreply.github.com>
2020-06-09 18:28:31 +0100

    Updated Docu

--- data/process_data.py
+++ data/process_data.py
@@ -1,3 +1,4 @@
+"""impot libraries"""
 import sys
 import pandas as pd
 import numpy as np
@@ -11,46 +12,57 @@ def load_data(messages_filepath, categories_filepath):
     """
     dfMessages = pd.read_csv(messages_filepath)
     dfCategories = pd.read_csv(categories_filepath)
-    df = dfMessages.merge(dfCategories, on=["id"])
+    dfCatastropheMessages = dfMessages.merge(dfCategories, on=["id"])
     return dfCatastropheMessages
 
 
-def clean_data(dfCatastropheMessages):
+def clean_data(dfCMessages):
     """
     Use dataframe read in by Load_data function
     Split up incomming messages by ;
     Select the column headers
-    Then use the last two values of the me 
+    Then use the last two values of the message to confirm whether a message is part of a category
+    Concatenate with original dataframe
+    Drop duplicate values
+    Remove any values that do not work on a 0-1 scale
+    Return cleaned dataframe
     """
-    categories = df["categories"].str.split(";", expand=True)
+    categories = dfCMessages["categories"].str.split(";", expand=True)
     row = categories.iloc[0]
     category_colnames = row.map(lambda x: str(x)[:-2])
     categories.columns = category_colnames
     for column in categories:
         categories[column] = categories[column].map(lambda x: str(x)[-1])
         categories[column] = pd.to_numeric(categories[column])
-    df = df.drop(columns=["categories"])
+    df = dfCMessages.drop(columns=["categories"])
     df = pd.concat([df, categories], axis=1)
     df = df.drop_duplicates()
-    df = df.loc[df['related'] != 2]
-    return df
+    dfClean = df.loc[df['related'] != 2]
+    return dfClean
   
-def save_data(df, database_filename):
+def save_data(dfClean, database_filename):
+    """
+    Save cleaned dataframe as a SQLite database
+    """
     engine = create_engine(f'sqlite:///{database_filename}')
-    df.to_sql('message', engine, index=False) 
+    dfClean.to_sql('message', engine, index=False) 
 
 
 def main():
+    """
+    Main function that kicks off all the other functions to clean and save the data
+    Contains status updates and initial instructions
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+        dfCMessages = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
-        dfFinal = clean_data(dfCatastrophyMessages)
+        dfFinal = clean_data(dfCMessages)
 
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(dfFinal, database_filepath)
