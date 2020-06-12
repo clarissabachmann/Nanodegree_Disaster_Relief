d6c9b828c2290e3aab70158c87c603a552318b81
clarissabachmann <51957412+clarissabachmann@users.noreply.github.com>
2020-06-12 08:55:48 +0100

    Sort out inconsistent ys

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -21,13 +21,14 @@ nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 
 def load_data(database_filepath):
     """
-    Load in data from cleaned database and create Y and X inputs
-    Also return category names to use with model evaluation
+    Load in data from cleaned database and create y and X inputs
+    Also return category
+    names to use with model evaluation
     """
     engine = create_engine("sqlite:///{}".format(database_filepath))
     df = pd.read_sql_query("SELECT * from message", engine)
     X=df['message']
-    y= df[['related', 'request', 'offer', 'aid_related', 'medical_help', 'medical_products', 'search_and_rescue', 'security', 'military',
+    y=df[['related', 'request', 'offer', 'aid_related', 'medical_help', 'medical_products', 'search_and_rescue', 'security', 'military',
       'child_alone', 'water', 'food', 'shelter', 'clothing', 'money', 'missing_people', 'refugees', 'death', 'other_aid', 'infrastructure_related',
       'transport', 'buildings', 'electricity', 'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure', 'weather_related',
       'floods', 'storm', 'fire', 'earthquake', 'cold', 'other_weather', 'direct_report']]
@@ -77,7 +78,7 @@ def build_model():
     cv = GridSearchCV(estimator=model, param_grid=parameters, cv=3)
     return cv
 
-def evaluate_model(model, X_test, Y_test, category_names):
+def evaluate_model(model, X_test, y_test, category_names):
     """
     Evaluate model and return the classification report
     """
@@ -101,8 +102,8 @@ def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X, y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, y_test = train_test_split(X, y, test_size=0.2)
         
         print('Building model...')
         model = build_model()
@@ -111,7 +112,7 @@ def main():
         model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, Y_test, X_test, category_names)
+        evaluate_model(model, y_test, X_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
